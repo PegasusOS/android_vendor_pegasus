d370f70ba12d857e09d9a4d43b8bb006c44c8d6d
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- config/BoardConfigPegasusOS.mk
+++ config/BoardConfigPegasusOS.mk
@@ -1,6 +1,11 @@
 # Kernel
 include vendor/pegasus/config/BoardConfigKernel.mk
 
+# Qcom-specific bits
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/pegasus/config/BoardConfigQcom.mk
+endif
+
 # Soong
 include vendor/pegasus/config/BoardConfigSoong.mk
 
