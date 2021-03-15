c81563915cd091d5d302784490195c64326a6c2b
althafvly <althafvly@gmail.com>
2021-03-15 11:17:08 +0530

    Drop tv support

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -22,11 +22,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_arm_ab.mk \
     $(LOCAL_DIR)/lineage_arm64_ab.mk \
     $(LOCAL_DIR)/lineage_x86_ab.mk \
-    $(LOCAL_DIR)/lineage_x86_64_ab.mk \
-    $(LOCAL_DIR)/lineage_tv_arm.mk \
-    $(LOCAL_DIR)/lineage_tv_arm64.mk \
-    $(LOCAL_DIR)/lineage_tv_x86.mk \
-    $(LOCAL_DIR)/lineage_tv_x86_64.mk
+    $(LOCAL_DIR)/lineage_x86_64_ab.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_arm-userdebug \
@@ -36,8 +32,4 @@ COMMON_LUNCH_CHOICES := \
     lineage_arm_ab-userdebug \
     lineage_arm64_ab-userdebug \
     lineage_x86_ab-userdebug \
-    lineage_x86_64_ab-userdebug \
-    lineage_tv_arm-userdebug \
-    lineage_tv_arm64-userdebug \
-    lineage_tv_x86-userdebug \
-    lineage_tv_x86_64-userdebug
+    lineage_x86_64_ab-userdebug
